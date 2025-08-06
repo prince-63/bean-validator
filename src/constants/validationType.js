1e7c329edca66a7821e93a1dc19dd6cd0914ce47
Prince Kumar Prasad <prince08833@gmail.com>
2025-08-06 13:01:52 +0530

    feat: add constant validator type

---
+++ src/constants/validationType.js
@@ -0,0 +1,38 @@
+const DIGITS = "digits";
+const EMAIL = "email";
+const MAX = "max";
+const MIN = "min";
+const NOT_BLANK = "notBlank";
+const NOT_EMPTY = "notEmpty";
+const NOT_NULL = "notNull";
+const PATTERN = "pattern";
+const SIZE = "size";
+const CREDIT_CARD_NUMBER = "creditCardNumber";
+const LENGTH = "length";
+const CURRENCY = "currency";
+const RANGE = "range";
+const URL = "url";
+const UNIQUE_ELEMENTS = "uniqueElements";
+const EAN = "ean";
+const ISBN = "isbn";
+
+module.exports = {
+    DIGITS,
+    EMAIL,
+    MAX,
+    MIN,
+    NOT_BLANK,
+    NOT_EMPTY,
+    NOT_NULL,
+    PATTERN,
+    SIZE,
+    CREDIT_CARD_NUMBER,
+    LENGTH,
+    CURRENCY,
+    RANGE,
+    URL,
+    UNIQUE_ELEMENTS,
+    EAN,
+    ISBN
+};
+
